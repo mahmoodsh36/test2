4b6dbcbcb7c7a2e8a86a54b964c4a163352e427a
mahmoodsh36 <33814416+mahmoodsh36@users.noreply.github.com>
2024-08-26 10:42:54 -0700

    Add files via upload

--- main.ps1
+++ main.ps1
@@ -1,4 +1,4 @@
-python -m venv venv
-. .\venv\Scripts\activate.ps1
+python -m venv C:\venv
+. C:\venv\Scripts\activate.ps1
 pip install tqdm flask pynput psutil paramiko rich requests
 python C:\main.py
